5a5d44e17d15fb63c699e7e9bfd6990534715a0d
Mike Sperber <sperber@deinprogramm.de>
2024-04-18 11:44:15 +0200

    Update.

--- src/data/defrecord.clj
+++ src/data/defrecord.clj
@@ -10,3 +10,14 @@
 (def parrot1 (->Parrot "hello!" 1))
 (def parrot2 (->Parrot "goodbye!" 2))
 
+;; (.weight dillo1) reflection warning
+;; (:weight dillo1)
+
+(defn run-over-animal
+  [animal]
+  (cond
+    (instance? Dillo animal)
+    (assoc animal :alive? false)
+
+    (instance? Parrot animal)
+    (assoc animal :sentence false)))
