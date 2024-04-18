b0bce1a6ef73cff524144716a3c281e48ff34fb5
Mike Sperber <sperber@deinprogramm.de>
2024-04-18 11:12:13 +0200

    Update.

--- src/data/active_data.clj
+++ src/data/active_data.clj
@@ -1,2 +1,18 @@
 (ns data.active-data)
 
+;; Datenmodellierung
+
+;; Tier (auf dem texanischen Highway) ist eins der folgenden:
+;; - Gürteltiere -ODER-
+;; - Papageien
+;; -> gemischte Daten 
+
+;; Gürteltier hat folgende Eigenschaften:
+;; - lebendig oder nicht -UND-
+;; - Gewicht
+;; -> zusammengesetzte Daten
+
+;; Papagei hat folgende Eigenschaften:
+;; - Satz -UND-
+;; - Gewicht
+
